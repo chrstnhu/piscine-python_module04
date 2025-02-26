dd819bed001053c1b22290e565ae095a76cc33a4
chrhu <chrhu@student.42.fr>
2025-02-26 13:26:33 +0100

    sort sport

--- ex05/HowManyMedalsByCountry.py
+++ ex05/HowManyMedalsByCountry.py
@@ -9,13 +9,17 @@ def how_many_medals_by_country(df, location):
     The function returns a dictionary of dictionaries giving the number and types of
     medals for each competition where the country delegation earned medals.
     
-    The keys of the main dictionary are the Olympic games’ years. In each year’s dictionary, 
-    the keys are ’G’, ’S’, ’B’ corresponding to the types of medals won.
+    The keys of the main dictionary are the Olympic games' years. In each year's dictionary, 
+    the keys are 'G', 'S', 'B' corresponding to the types of medals won.
     
     Duplicated medals per team games should be handled and not counted twice.
     
     Hint: You may find this list to be of some use.
     '''
+    team_sports = ['Basketball', 'Football', 'Tug-Of-War', 'Badminton', 'Sailing',
+        'Handball', 'Water Polo', 'Hockey', 'Rowing', 'Bobsleigh', 'Softball',
+        'Volleyball', 'Synchronized Swimming', 'Baseball', 'Rugby Sevens',
+        'Rugby', 'Lacrosse', 'Polo']
 
     # Find location
     location_data = df.loc[(df['Team'] == location)]
@@ -29,19 +33,24 @@ def how_many_medals_by_country(df, location):
 
         year_data = location_data[(location_data['Year'] == year)]
 
-        gold = year_data[(year_data['Medal'] == 'Gold')].shape[0]
-        silver = year_data[(year_data['Medal'] == 'Silver')].shape[0]
-        bronze = year_data[(year_data['Medal'] == 'Bronze')].shape[0]
-
-        if gold == 0 and silver == 0 and bronze == 0:
-            continue
+        medals_count[year] = {}
         
-        # Store the medals count in the dictionary
-        medals_count[year] = {'G': gold, 'S': silver, 'B': bronze}
+        for sport in sorted(year_data['Sport'].unique()):
+            sport_data = year_data[(year_data['Sport'] == sport)]
+
+            gold = year_data[(year_data['Medal'] == 'Gold')].shape[0]
+            silver = year_data[(year_data['Medal'] == 'Silver')].shape[0]
+            bronze = year_data[(year_data['Medal'] == 'Bronze')].shape[0]
+
+            if gold > 0 or silver > 0 or bronze > 0:
+                medals_count[year][sport] = {'G': gold, 'S': silver, 'B': bronze}
+            
+        
+        if not medals_count[year]:
+            del medals_count[year]
     
     # Sort the dictionary
-    medals_count_keys = list(medals_count.keys())
-    medals_count_keys.sort()
+    medals_count_keys = sorted(medals_count.keys())
 
     sorted_medals_count = {i: medals_count[i] for i in medals_count_keys}
 
